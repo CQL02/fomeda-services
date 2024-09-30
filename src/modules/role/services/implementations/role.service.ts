b27775eebec1136b3c7f0666f3dfd54c615623fd
MoneyCheongJx <moneycheongjx2002@gmail.com>
2024-09-30 21:54:01 +0800

    fix(role) fix role management created_by & last_updated_by bug

--- src/modules/role/services/implementations/role.service.ts
+++ src/modules/role/services/implementations/role.service.ts
@@ -15,11 +15,86 @@ export class RoleService implements IRoleService {
   }
 
   async findAllRoles(): Promise<RoleDto[]> {
-    return this.roleRepository.findAll();
+    const pipeline = [
+      {
+        $lookup: {
+          from: 'user',
+          localField: 'created_by',
+          foreignField: 'user_id',
+          as: 'creator',
+        },
+      },
+      {
+        $unwind: {
+          path: '$creator',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'user',
+          localField: 'last_updated_by',
+          foreignField: 'user_id',
+          as: 'updater',
+        },
+      },
+      {
+        $unwind: {
+          path: '$updater',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          created_by: { $ifNull: ['$creator.username', '$created_by'] },
+          last_updated_by: { $ifNull: ['$updater.username', '$last_updated_by'] },
+        },
+      },
+      { $unset: ['creator', 'updater'] }
+    ];
+    return await this.roleRepository.aggregate(pipeline);
   }
 
   async findAllActiveRoles(): Promise<RoleDto[]> {
-    return this.roleRepository.findAllByFilter({is_active: true});
+    const pipeline = [
+      { $match: { is_active: true } },
+      {
+        $lookup: {
+          from: 'user',
+          localField: 'created_by',
+          foreignField: 'user_id',
+          as: 'creator',
+        },
+      },
+      {
+        $unwind: {
+          path: '$creator',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: 'user',
+          localField: 'last_updated_by',
+          foreignField: 'user_id',
+          as: 'updater',
+        },
+      },
+      {
+        $unwind: {
+          path: '$updater',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          created_by: { $ifNull: ['$creator.username', '$created_by'] },
+          last_updated_by: { $ifNull: ['$updater.username', '$last_updated_by'] },
+        },
+      },
+      { $unset: ['creator', 'updater'] }
+    ];
+    return this.roleRepository.aggregate(pipeline);
   }
 
   async updateRole(id: string, roleDto: RoleDto): Promise<RoleDto> {
