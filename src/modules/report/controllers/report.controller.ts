77e9f509be35e38fc76074fe0d6391e495162a2a
qilin <chungqilin02@gmail.com>
2024-11-02 19:58:18 +0800

    feat(report): add user_id using auth when update product report

--- src/modules/report/controllers/report.controller.ts
+++ src/modules/report/controllers/report.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Inject, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Post, Put, Query, Req, UseGuards } from "@nestjs/common";
 import { ReportService } from "../services/implementations/report.service";
 import { IReportService } from "../services/interfaces/report.service.interface";
 import { ReportDto } from "../dtos/report.dto";
 import { ReportFilterDto } from "../dtos/report-filter.dto";
+import { AuthenticationGuard } from "../../authentication/passport/authentication.guard";
+import { Request } from "express";
 
 @Controller("report")
 export class ReportController {
@@ -20,9 +22,10 @@ export class ReportController {
     return await this.reportService.getReportDetails(id)
   }
 
+  @UseGuards(AuthenticationGuard)
   @Post("get-supplier-report-list-by-filter")
-  async getSupplierReportListByFilter(@Body() filterDto: ReportFilterDto): Promise<ReportDto[]> {
-    return await this.reportService.getSupplierReportListByFilter(filterDto);
+  async getSupplierReportListByFilter(@Req() req: Request, @Body() filterDto: ReportFilterDto): Promise<ReportDto[]> {
+    return await this.reportService.getSupplierReportListByFilter(req, filterDto);
   }
 
   @Post("get-admin-report-list-by-filter")
@@ -30,8 +33,9 @@ export class ReportController {
     return await this.reportService.getAdminReportListByFilter(filterDto);
   }
 
+  @UseGuards(AuthenticationGuard)
   @Put("update-report-status")
-  async updateReportStatus(@Query("id") id: string, @Body() updateReportDto: ReportDto): Promise<boolean> {
-    return await this.reportService.updateReportStatus(id, updateReportDto);
+  async updateReportStatus(@Req() req: Request, @Query("id") id: string, @Body() updateReportDto: ReportDto): Promise<boolean> {
+    return await this.reportService.updateReportStatus(req, id, updateReportDto);
   }
 }
