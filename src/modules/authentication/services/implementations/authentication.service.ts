4187ec7f408820e720c74850b7c66dcdef3d0087
MoneyCheongJx <moneycheongjx2002@gmail.com>
2024-11-14 18:15:13 +0800

    fix(auth) Add domain

--- src/modules/authentication/services/implementations/authentication.service.ts
+++ src/modules/authentication/services/implementations/authentication.service.ts
@@ -756,6 +756,7 @@ export class AuthenticationService implements IAuthenticationService {
           sameSite: 'none',
           path: '/',
           maxAge: 10 * 60 * 1000,
+          domain: '.onrender.com',
         });
 
         res.json({
