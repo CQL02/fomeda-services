a27f66a9c63952f1811e1de35ccbd84bb6f00e41
qilin <chungqilin02@gmail.com>
2024-11-29 20:48:40 +0800

    feat(test): update category test example

--- test/category.e2e.ts
+++ test/category.e2e.ts
@@ -94,94 +94,98 @@ export const categoryTest = () => {
             });
         });
 
-        describe("UTC_PCT_03 Create General Specification", () => {
-          const data = {
-            cat_type: "WARRANTY",
-            subcat_spec_name: "General Warranty",
-            is_active: false,
-            allow_input: true,
-            is_required: true,
-            prefix: "",
-            suffix: "year(s)",
-            field_type: "NUMERIC",
-            is_score_contributed: true,
-            rating_score: [
-              { action: "MORE_THAN", value: "5", score: 10 },
-              { action: "MORE_THAN", value: "3", score: 8 }
-            ]
-          };
+      });
 
-          it("General Specification created successfully", async () => {
-            return request(app.getHttpServer())
-              .post("/category/create-general-specification")
-              .set("Cookie", cookies)
-              .send(data)
-              .expect((res) => {
-                expect(res.body).toBeInstanceOf(Object);
-              });
-          });
+      describe("UTC_PCT_03 Create General Specification", () => {
+        const data = {
+          cat_type: "WARRANTY",
+          subcat_spec_name: "General Warranty",
+          is_active: false,
+          allow_input: true,
+          is_required: true,
+          prefix: "",
+          suffix: "year(s)",
+          field_type: "NUMERIC",
+          is_score_contributed: true,
+          rating_score: [
+            { action: "MORE_THAN", value: "5", score: 10 },
+            { action: "MORE_THAN", value: "3", score: 8 }
+          ]
+        };
 
-          it("Specification name already existed", async () => {
-            return request(app.getHttpServer())
-              .post("/category/create-general-specification")
-              .set("Cookie", cookies)
-              .send(data)
-              .expect((res) => {
-                expect(res.body).toBeInstanceOf(Object);
-              });
-          });
+        it("General Specification created successfully", async () => {
+          return request(app.getHttpServer())
+            .post("/category/create-general-specification")
+            .set("Cookie", cookies)
+            .send(data)
+            .expect((res) => {
+              expect(res.body).toBeInstanceOf(Object);
+            });
+        });
 
-          it("Specification name is missing", async () => {
-            return request(app.getHttpServer())
-              .post("/category/create-general-specification")
-              .set("Cookie", cookies)
-              .send({ ...data, subcat_spec_name: "" })
-              .expect((res) => {
-                expect(res.body).toBeInstanceOf(Object);
-              });
-          });
+        it("Specification name already existed", async () => {
+          return request(app.getHttpServer())
+            .post("/category/create-general-specification")
+            .set("Cookie", cookies)
+            .send(data)
+            .expect((res) => {
+              expect(res.body).toBeInstanceOf(Object);
+            });
+        });
+
+        it("Specification name is missing", async () => {
+          return request(app.getHttpServer())
+            .post("/category/create-general-specification")
+            .set("Cookie", cookies)
+            .send({ ...data, subcat_spec_name: "" })
+            .expect((res) => {
+              expect(res.body).toBeInstanceOf(Object);
+            });
         });
       });
 
-      it("/category/find-all-category (GET)", async () => {
-        return request(app.getHttpServer())
-          .get("/category/find-all-category")
-          .expect((res) => {
-            expect(res.body).toBeInstanceOf(Array);
-          });
-      });
 
-      it("/category/find-all-active-categories (GET)", async () => {
-        return request(app.getHttpServer())
-          .get("/category/find-all-active-categories")
-          .expect((res) => {
-            expect(res.body).toBeInstanceOf(Array);
-          });
-      });
-
-      it("/category/update-category (PUT)", async () => {
-        const id = "CAT000000000009";
-        const data = { cat_name: "Demo Category" };
-
-        return request(app.getHttpServer())
-          .put(`/category/update-category?id=${id}`)
-          .set("Cookie", cookies)
-          .send(data)
-          .expect((res) => {
-            expect(res.body).toBeInstanceOf(Object);
-          });
-      });
-
-      it("/category/delete-category (DELETE)", async () => {
-        const id = "CAT000000000016";
-
-        return request(app.getHttpServer())
-          .delete(`/category/delete-category?id=${id}`)
-          .set("Cookie", cookies)
-          .expect((res) => {
-            expect(res.body).toBeInstanceOf(Object);
-          });
-      });
     });
+
+    it("/category/find-all-category (GET)", async () => {
+      return request(app.getHttpServer())
+        .get("/category/find-all-category")
+        .expect((res) => {
+          expect(res.body).toBeInstanceOf(Array);
+        });
+    });
+
+    it("/category/find-all-active-categories (GET)", async () => {
+      return request(app.getHttpServer())
+        .get("/category/find-all-active-categories")
+        .expect((res) => {
+          expect(res.body).toBeInstanceOf(Array);
+        });
+    });
+
+    it("/category/update-category (PUT)", async () => {
+      const id = "CAT000000000009";
+      const data = { cat_name: "Demo Category" };
+
+      return request(app.getHttpServer())
+        .put(`/category/update-category?id=${id}`)
+        .set("Cookie", cookies)
+        .send(data)
+        .expect((res) => {
+          expect(res.body).toBeInstanceOf(Object);
+        });
+    });
+
+    it("/category/delete-category (DELETE)", async () => {
+      const id = "CAT000000000016";
+
+      return request(app.getHttpServer())
+        .delete(`/category/delete-category?id=${id}`)
+        .set("Cookie", cookies)
+        .expect((res) => {
+          expect(res.body).toBeInstanceOf(Object);
+        });
+    });
+
   });
 };
