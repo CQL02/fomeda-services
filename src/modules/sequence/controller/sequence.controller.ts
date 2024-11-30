aa2e789dd8cb7c4e46789c47b01121a81f3d389c
qilin <chungqilin02@gmail.com>
2024-11-30 16:24:02 +0800

    feat(sequence): add get last sequence id

--- src/modules/sequence/controller/sequence.controller.ts
+++ src/modules/sequence/controller/sequence.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Put } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Put } from "@nestjs/common";
 import { SequenceDto } from '../dtos/sequence.dto';
 import { SequenceService } from '../services/implementations/sequence.services';
 import { ISequenceService } from "../services/interfaces/sequence.service.interface";
@@ -10,5 +10,10 @@ export class SequenceController {
   @Put('generate-id')
   async generateId(@Body() sequenceDto: SequenceDto): Promise<string> {
     return await this.sequenceService.generateId(sequenceDto.prefix);
+  }
+
+  @Get('get-last-id')
+  async getLastId(@Body() sequenceDto: SequenceDto): Promise<string> {
+    return await this.sequenceService.getLastId(sequenceDto.prefix);
   }
 }
--- src/modules/sequence/services/interfaces/sequence.service.interface.ts
+++ src/modules/sequence/services/interfaces/sequence.service.interface.ts
@@ -1,3 +1,4 @@
 export interface ISequenceService {
   generateId(prefix: string): Promise<string>;
+  getLastId(prefix: string): Promise<string>;
 }
